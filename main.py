a820223eb592e6011e94898eef65a69087a6786a
Tx0sh1 <clydemojura@protonmail.com>
2025-01-28 20:30:57 +0200

    refactor code

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        return file_contents.lower()
+    
+def count_characters(letters):
+    char = {}      
+    for letter in letters:
+        if letter.isalpha():
+            char[letter] = char.get(letter, 0) + 1
+    return char
+
+letters = main()
+char_count = count_characters(letters)  
+
+print(char_count)
+
+def formatted_output():
+    for char in char_count:
+        print(char)
         
-main()
-        
+formatted_output()
+               
